e6ea9a4cc30104a05e4fe9f1652deaabc956b6b3
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-06-25 23:40:04 +0700

    My Learning Package (Kotlin)

--- src/AmigoScope/variable.kt
+++ src/AmigoScope/variable.kt
@@ -3,5 +3,22 @@ package AmigoScope
 fun main(args : Array<String>) {
     println("Hello World")
     println("How are you ?")
+
+    //Declaration variable
+    var name : String = "Ezzie"
+    var age : Int = 25;
+    println(name)
+    println(age)
+    println("Name : ${name} Age: ${age}")
+    println("Name : $name , Age : $age")
+
+   // int number = 19;// in java
+    var number : Int = 10;
+    var l : Long = 100L;
+    var d : Double = 3.14;
+    var f : Float = 3.14F;
+    var b : Boolean = true;
+    var s : String = "Amigoscode"
+
 }
 
