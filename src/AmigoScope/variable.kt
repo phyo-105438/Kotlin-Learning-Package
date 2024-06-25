1350b35ce96efb0f0e900ed005189221455fe02f
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-06-25 23:40:04 +0700

    My Learning Package (Kotlin)

--- src/AmigoScope/variable.kt
+++ src/AmigoScope/variable.kt
@@ -13,12 +13,30 @@ fun main(args : Array<String>) {
     println("Name : $name , Age : $age")
 
    // int number = 19;// in java
-    var number : Int = 10;
+    var n : Int = 10;
     var l : Long = 100L;
     var d : Double = 3.14;
     var f : Float = 3.14F;
     var b : Boolean = true;
-    var s : String = "Amigoscode"
+    var s : String = "Amigoscode";
+    var c : Char = 'A';
+
+    //Any data type
+    var number : Any = 10 // You can add any data type
+
+    //Type Inference
+    var n1 = 10;
+    var l1 = 100L;
+    var d1 = 3.14;
+
+    // Var vs Val
+
+    var num1 = 100;
+    val num2 = 200;
+
+    num1 = 123;
+
+
 
 }
 
