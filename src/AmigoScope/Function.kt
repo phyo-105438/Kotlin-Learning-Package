8a106fc41ab3a39edceca3e7282dc51a9ec11dff
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-07-06 11:04:03 +0700

    My Learning Package (Kotlin)

---
+++ src/AmigoScope/Function.kt
@@ -0,0 +1,12 @@
+package AmigoScope
+
+class Function {
+}
+
+fun main(){
+    greet();
+}
+
+fun greet() : Unit{// default include public and unit like public void in java
+    print("Hello Kotlin");
+}
