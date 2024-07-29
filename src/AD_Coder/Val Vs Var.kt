62b416bde0ac4bff0a848fe71f1ea5b5324ddfd2
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-07-29 18:00:32 +0700

    My Learning Package (Kotlin)

--- src/AD_Coder/Val Vs Var.kt
+++ src/AD_Coder/Val Vs Var.kt
@@ -3,5 +3,10 @@ package AD_Coder
 class `Val Vs Var` {
 }
 fun main(){
-
+    //Var vs Val
+    var a : Int = 1 // immutable variable
+    val b : Int = 5 // mutable variable
+    a = 2
+    println(a)
+    println(b)
 }
