dfc0913f04f3075269218d1c99162b2ef29f2d96
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-07-29 18:54:58 +0700

    My Learning Package (Kotlin)

--- src/AD_Coder/Operator.kt
+++ src/AD_Coder/Operator.kt
@@ -2,3 +2,28 @@ package AD_Coder
 
 class Operator {
 }
+
+fun main (){
+    //Assignment Operator + - * / %
+    var a : Int = 4
+    var b : Int = 5
+    var c = a % b
+    println(c)
+    //Compound operator
+    var one : String = "Android"
+    var two : String = "Kotlin"
+    var three = one + two
+    println(three)
+
+    //Relational > < >= <= == !=
+    var x = 3
+    var y = 1
+    //If else statement
+    //Bitwise Operator || &&
+    if(x>y && x!=y){
+        println("True")
+    }
+    else{
+        println("False")
+    }
+}
