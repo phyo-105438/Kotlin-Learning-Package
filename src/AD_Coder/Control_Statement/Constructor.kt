5ca17e044c3f604a739df8956fe5bf7de3afe3b5
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-08-04 17:23:30 +0700

    My Learning Package (Kotlin)

--- src/AD_Coder/Control_Statement/Constructor.kt
+++ src/AD_Coder/Control_Statement/Constructor.kt
@@ -2,3 +2,13 @@ package AD_Coder.Control_Statement
 
 class Constructor {
 }
+fun main(){
+    var s1 = Student_1("Ezzie", "ez123")
+    s1.n
+}
+
+class Student_1 (var name : String , var mail : String ){
+    constructor(nameRes : String , mailRes String) : this(name,mail){
+
+    }
+}
