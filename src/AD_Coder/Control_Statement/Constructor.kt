642eb763844c38ecadf365b5d53deb02ef529aef
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-08-04 17:36:45 +0700

    My Learning Package (Kotlin)

--- src/AD_Coder/Control_Statement/Constructor.kt
+++ src/AD_Coder/Control_Statement/Constructor.kt
@@ -4,11 +4,19 @@ class Constructor {
 }
 fun main(){
     var s1 = Student_1("Ezzie", "ez123")
-    s1.n
+    println(s1.name)
+    println(s1.mail)
+    println(s1.rollNo)
+
+    var s2 = Student_1("Android", "android@gmail.com","AC_3000")
+    println(s2.name)
+    println(s2.mail)
+    println(s2.rollNo)
 }
 
 class Student_1 (var name : String , var mail : String ){
-    constructor(nameRes : String , mailRes String) : this(name,mail){
-
+    var rollNo : String = ""
+    constructor(nameRes : String , mailRes : String , rollNoRes : String) : this(nameRes,mailRes){
+        this.rollNo = rollNoRes
     }
 }
