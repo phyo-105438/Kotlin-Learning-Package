106819a9cd5ac2802c7d93dc327f2c50a9a0af7c
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-08-04 14:25:45 +0700

    My Learning Package (Kotlin)

--- src/AD_Coder/Control_Statement/Function.kt
+++ src/AD_Coder/Control_Statement/Function.kt
@@ -2,3 +2,19 @@ package AD_Coder.Control_Statement
 
 class Function {
 }
+fun main(){
+    hello()
+}
+//Simple or no parameter function
+fun hello(){
+    println("This is an hello function")
+    var a = 5
+    var b = 7
+    var c = a + b
+    if(c%2 == 0){
+        println("c is even number")
+    }
+    else{
+        println("c is odd number")
+    }
+}
