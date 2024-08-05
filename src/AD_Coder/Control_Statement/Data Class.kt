0fcf9f77aaa865cfcdf7d4ea43b5411dd0d3e21f
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-08-05 16:55:51 +0700

    My Learning Package (Kotlin)

--- src/AD_Coder/Control_Statement/Data Class.kt
+++ src/AD_Coder/Control_Statement/Data Class.kt
@@ -2,3 +2,19 @@ package AD_Coder.Control_Statement
 
 class `Data Class` {
 }
+
+fun main(){
+    var s1 = Student_3("Mg Mg", "Yangon")
+    s1.show()
+    var loc = s1.info()
+    println(loc)
+}
+
+data class Student_3(var name : String , var location : String){ // data class must include argument
+    fun show(){
+        println("Name = $name , Location = $location")
+    }
+    fun info() : String {
+        return location
+    }
+}
