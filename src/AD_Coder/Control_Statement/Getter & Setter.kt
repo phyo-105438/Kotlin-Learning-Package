de4abc406e91dd857136c7a87d0ba0cb449e736b
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-08-05 16:36:57 +0700

    My Learning Package (Kotlin)

--- src/AD_Coder/Control_Statement/Getter & Setter.kt
+++ src/AD_Coder/Control_Statement/Getter & Setter.kt
@@ -6,15 +6,25 @@ fun main(){
     var s1 = Student_2()
     s1.name = "Ezzie" // set
     println(s1.name)  // get
+    var e1 = Employee()
+    e1.info("Mg Mg")
+    println(e1.name)
 
 }
 class Student_2(){
     var name : String = ""
-        get() = field //Getter
+        get() = field.uppercase() //Getter
 
         set(value){ // value = "Ezzie"
             field = value
         }
+}
+//Getter and setter using inner function
+class Employee(){
+    var name : String = ""
+    fun info(name : String){
+        this.name = name
+    }
 }
 
 
