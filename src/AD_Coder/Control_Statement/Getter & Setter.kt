0827aef6fb5db63657263ac7438444c8c968a345
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-08-05 16:22:50 +0700

    My Learning Package (Kotlin)

--- src/AD_Coder/Control_Statement/Getter & Setter.kt
+++ src/AD_Coder/Control_Statement/Getter & Setter.kt
@@ -4,12 +4,17 @@ class `Getter & Setter` {
 }
 fun main(){
     var s1 = Student_2()
-    s1.name = "Ezzie"
-    println(s1.name)
-    //Getter
+    s1.name = "Ezzie" // set
+    println(s1.name)  // get
+
 }
 class Student_2(){
     var name : String = ""
+        get() = field //Getter
+
+        set(value){ // value = "Ezzie"
+            field = value
+        }
 }
 
 
