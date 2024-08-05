7e5447a682f013f11d5bd485c7068665d5efd542
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-08-05 17:24:06 +0700

    My Learning Package (Kotlin)

--- src/AD_Coder/Control_Statement/Sealed Class.kt
+++ src/AD_Coder/Control_Statement/Sealed Class.kt
@@ -2,3 +2,16 @@ package AD_Coder.Control_Statement
 
 class `Sealed Class` {
 }
+sealed class Error( var message : String){
+    class Network() : Error("Network Error")
+    class Database() : Error("Database Error")
+    class API() : Error("API Error")
+    class Unknown() : Error("Unknown")
+}
+
+fun main(){
+    var error = listOf(Error.Network(), Error.Database(), Error.API(), Error.Unknown())
+    error.forEach {
+        println(it.message)
+    }
+}
