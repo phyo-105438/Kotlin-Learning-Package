5cb9b5947463316b670469fb053a05b2d094bfe0
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-08-05 19:00:34 +0700

    My Learning Package (Kotlin)

--- src/AD_Coder/Control_Statement/Nested & InnerClass.kt
+++ src/AD_Coder/Control_Statement/Nested & InnerClass.kt
@@ -2,3 +2,19 @@ package AD_Coder.Control_Statement
 
 class `Nested & InnerClass` {
 }
+class Car(){
+    var model : String = ""
+    inner class Name(){
+        fun show(){
+
+        }
+    }
+    fun info(){
+
+    }
+}
+fun main(){
+    var car1 = Car()
+    car1.model= "Mercedes"
+    println(car1.model)
+}
